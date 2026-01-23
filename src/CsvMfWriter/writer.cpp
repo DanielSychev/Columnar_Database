d653e9fd82191737eb01740799c1e2f8d98fae27
Daniil Sychev <dasychev@edu.hse.ru>
2026-01-23 21:15:39 +0300

    Fixed reader and writer namings, added some extra tests for reader and writer

---
+++ src/CsvMfWriter/writer.cpp
@@ -0,0 +1,42 @@
+#include <CsvMfWriter/writer.h>
+#include <cinttypes>
+#include <memory>
+
+Writer::Writer(std::ostream& ss, char delimetr) : out_(ss), delimetr_(delimetr) {
+}
+
+void Writer::CheckFlag(bool fl) {
+    if (!fl) {
+        out_ << ',';
+    } else {
+        out_ << '\n';
+    }
+}
+
+void Writer::WriteElem(int64_t x, bool fl) {
+    out_ << x;
+    CheckFlag(fl);
+}
+
+void Writer::WriteElem(const std::string& s, bool fl) {
+    if (s.find(delimetr_) == std::string::npos &&
+    s.find('"') == std::string::npos) {
+        out_ << s;
+        CheckFlag(fl);
+        return;
+    }
+    out_ << '"';
+    for (auto& c : s) {
+        if (c == '"') {
+            out_ << R"("")";
+        } else {
+            out_ << c;
+        }
+    }
+    out_ << '"';
+    CheckFlag(fl);
+}
+
+size_t Writer::TellPos() {
+    return out_.tellp();
+}
---
+++ src/Tests/TestWriter.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+#include "CsvMfWriter/writer.h"
+
+TEST(MF_Writer, WriteElemInt) {
+    std::stringstream s;
+    Writer w(s);
+    w.WriteElem(1, false);
+    w.WriteElem(2, false);
+    w.WriteElem(3, true);
+    EXPECT_EQ(s.str(), "1,2,3\n");
+}
+
+TEST(MF_Writer, WriteElemBasic) {
+    std::stringstream s;
+    Writer w(s);
+    w.WriteElem("Hello", false);
+    w.WriteElem("World", true);
+    EXPECT_EQ(s.str(), "Hello,World\n");
+}
+
+TEST(MF_Writer, WriteElemQuotes) {
+    std::stringstream s;
+    Writer w(s);
+    w.WriteElem(R"(He said "Hello")", false);
+    w.WriteElem(R"(She said "Hi")", true);
+    EXPECT_EQ(s.str(), R"("He said ""Hello""","She said ""Hi""")""\n");
+}
+
+TEST(MF_Writer, WriteElemDelimetr) {
+    std::stringstream s;
+    Writer w(s);
+    w.WriteElem("Hello, World!", true);
+    EXPECT_EQ(s.str(), R"("Hello, World!")""\n");
+}
+
+TEST(MF_Writer, WriteElemHardTest) {
+    std::stringstream s;
+    Writer w(s);
+    w.WriteElem(R"("Start quote)", false);
+    w.WriteElem(R"(End quote")", false);
+    w.WriteElem(R"(Mixed"quote"here)", true);
+    EXPECT_EQ(s.str(), R"("""Start quote","End quote""","Mixed""quote""here")""\n");
+}
+
+TEST(MF_Writer, BinaryWriteInt64) {
+    std::stringstream s;
+    Writer w(s);
+    int64_t number = 1904921021041903;
+    w.BinaryWrite(number);
+    int64_t value;
+    s.read(reinterpret_cast<char*>(&value), sizeof(int64_t));
+    EXPECT_EQ(number, value);
+}
+
+TEST(MF_Writer, BinaryWriteUInt32) {
+    std::stringstream s;
+    Writer w(s);
+    uint32_t number = 1904921021;
+    w.BinaryWrite(number);
+    uint32_t value;
+    s.read(reinterpret_cast<char*>(&value), sizeof(uint32_t));
+    EXPECT_EQ(number, value);
+}
