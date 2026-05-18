377f30bb8f7b97bbd31e139746cabc5d33810525
Daniil Sychev <dasychev@edu.hse.ru>
2026-05-19 01:34:54 +0300

    Add pkg-config fallback for RE2 on Ubuntu 24.04

--- cmake/dependencies_package.cmake
+++ cmake/dependencies_package.cmake
@@ -2,7 +2,15 @@ if(APPLE)
     list(APPEND CMAKE_PREFIX_PATH /opt/homebrew)
 endif()
 
-find_package(re2 REQUIRED)
+find_package(re2 QUIET)
+
+if(NOT re2_FOUND)
+    find_package(PkgConfig REQUIRED)
+    pkg_check_modules(RE2 REQUIRED re2)
+    add_library(re2::re2 INTERFACE IMPORTED)
+    target_include_directories(re2::re2 INTERFACE ${RE2_INCLUDE_DIRS})
+    target_link_libraries(re2::re2 INTERFACE ${RE2_LINK_LIBRARIES})
+endif()
 
 if(BUILD_TESTING)
     find_package(GTest REQUIRED)
