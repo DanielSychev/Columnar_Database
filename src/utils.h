1764ecac14d91a27d5f983de8f259a8c51abe308
Daniil Sychev <dasychev@edu.hse.ru>
2026-05-29 15:42:34 +0300

    Changed constant

--- src/utils.h
+++ src/utils.h
@@ -7,7 +7,7 @@
 #include <type_traits>
 
 namespace Constants {
-    const size_t BATCH_SIZE = 2<<14; // 16K rows
+    const size_t BATCH_SIZE = 1<<15; // 32K rows
     const size_t MAX_COLUMN_COUNT = UINT32_MAX;
     const size_t ORDER_BY_NO_LIMIT = SIZE_MAX;
 }
